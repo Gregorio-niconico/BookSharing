f2e0eade1297b4adf6f36fdd12ef5ee399d53366
yangzhenqi <gregorio_o@163.com>
2019-10-11 18:00:10 +0800

    Third Commit

--- app/src/main/java/com/example/booksharing/MainActivity.java
+++ app/src/main/java/com/example/booksharing/MainActivity.java
@@ -3,12 +3,9 @@ package com.example.booksharing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.booksharing.database.RegisterActivity;
 
 import org.litepal.LitePal;
 
--- app/src/main/java/com/example/booksharing/RegisterActivity.java
+++ app/src/main/java/com/example/booksharing/RegisterActivity.java
@@ -1,4 +1,4 @@
-package com.example.booksharing.database;
+package com.example.booksharing;
 
 import android.os.Bundle;
 
@@ -9,15 +9,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 import com.example.booksharing.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button confirmButton=(Button)findViewById(R.id.button_confirm);
+        Button cancelButton=(Button)findViewById(R.id.button_cancel);
+        confirmButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+        }
+    }
 }
